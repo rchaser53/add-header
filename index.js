20558263495b364f56725cc6ad1b97d338d9a34c
rchaser53 <tayoshizawa29@gmail.com>
2019-02-12 10:27:51 +0900

    add option c to customize header string

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const fs = require("fs");
-const yargs = require("yargs");
+const fs = require('fs')
+const yargs = require('yargs')
 
-const HeaderStr = "#!/usr/bin/env node\n";
+const HeaderStr = '#!/usr/bin/env node\n'
 
-const args = yargs.command("*", "add-header [src] [dist]").option("-c", {
-  describe: "config file path",
-  type: "string"
-}).argv;
+const args = yargs.command('*', 'add-header [src] [dist]').option('-c', {
+  describe: 'config file path',
+  type: 'string'
+}).argv
 
 if (args._.length <= 1) {
-  throw new Error("no src or dist path ");
+  throw new Error('no src or dist path ')
 }
-const [src, dist] = args._;
 
 try {
-  const srcStream = fs.createReadStream(src, 'utf8');
-  const destStream = fs.createWriteStream(dist, 'utf8');
+  const headerStr = args.c != null ? fs.readFileSync(args.c, { encoding: 'utf8' }) : HeaderStr
+
+  const [src, dist] = args._
+
+  const srcStream = fs.createReadStream(src, 'utf8')
+  const destStream = fs.createWriteStream(dist, 'utf8')
   destStream.on('open', () => {
-    const chunk = Buffer.from(HeaderStr, 'utf8')
+    const chunk = Buffer.from(headerStr, 'utf8')
     destStream.write(chunk)
   })
-  
-  srcStream.on('data', chunk => destStream.write(chunk));
-  srcStream.on('end', () => destStream.end());
+
+  srcStream.on('data', (chunk) => destStream.write(chunk))
+  srcStream.on('end', () => destStream.end())
 } catch (err) {
-  throw new Error(err);
+  throw new Error(err)
 }
